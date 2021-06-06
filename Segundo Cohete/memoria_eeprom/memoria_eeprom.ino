d0a43bd1e918f9b0084646d2235c473b1255a8dd
Johan Fernando Romo Eligio <feryohan343@gmail.com>
2021-06-05 20:50:38 -0500

    se añadio un programa para poner los estados de los sensores en 1 (funciona)

---
+++ Segundo Cohete/memoria_eeprom/memoria_eeprom.ino
@@ -0,0 +1,31 @@
+#include <EEPROM.h>
+
+byte uno = 1;
+byte dos = 1;
+byte tres = 1;
+byte cuatro = 1;
+byte cinco = 1;
+byte seis = 1;
+byte siete = 1;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  EEPROM.put(1,uno);
+  EEPROM.put(2,dos);
+  EEPROM.put(3,tres);
+  EEPROM.put(4,cuatro);
+  EEPROM.put(5,cinco);
+  EEPROM.put(6,seis);
+  EEPROM.put(7,siete);
+
+  Serial.println(EEPROM.read(0));
+  Serial.println(EEPROM.read(1));
+  Serial.println(EEPROM.read(2));
+  Serial.println(EEPROM.read(3));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
